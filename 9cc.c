7d4c132ae750ea5f3def272f315b4360f165e99e
norick1701@gmail.com <norick1701@gmail.com>
2018-11-17 10:22:42 +0900

    sixth commit

--- 9cc.c
+++ 9cc.c
@@ -16,9 +16,30 @@ typedef struct {
   char *input; // トークン文字列（エラーメッセージ用）
 } Token;
 
+enum {
+  ND_NUM = 256, // 整数のノード型
+};
+
+typedef struct Node {
+  int ty;           // 演算子かND_NUM
+  struct Node *lhs; //  左辺
+  struct Node *rhs; //  右辺
+  int val;          //  tyがND_NUMの場合のみ使う
+} Node;
+
+Node *new_node();
+Node *new_node_num();
+Node *expr();
+Node *mul();
+Node *term();
+void error(char *, char *);
+void gen(Node *);
+
 // トークナイズした結果のトークン列はこの配列に保存する
 // １００個以上のトークンは来ないものとする
 Token tokens[100];
+
+int pos;
 
 // pが指している文字列をトークンに分割してtokensに保存する
 void tokenize(char *p) {
@@ -30,7 +51,8 @@ void tokenize(char *p) {
       continue;
     }
 
-    if (*p == '+' || *p == '-') {
+    if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' ||
+        *p == ')') {
       tokens[i].ty = *p;
       tokens[i].input = p;
       i++;
@@ -54,10 +76,105 @@ void tokenize(char *p) {
   tokens[i].input = p;
 }
 
+Node *new_node(int op, Node *lhs, Node *rhs) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = op;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *expr() {
+  Node *lhs = mul();
+
+  if (tokens[pos].ty == '+') {
+    pos++;
+    return new_node('+', lhs, expr());
+  }
+
+  if (tokens[pos].ty == '-') {
+    pos++;
+    return new_node('-', lhs, expr());
+  }
+  return lhs;
+}
+
+Node *mul() {
+  Node *lhs = term();
+
+  if (tokens[pos].ty == '*') {
+    pos++;
+    return new_node('*', lhs, mul());
+  }
+
+  if (tokens[pos].ty == '/') {
+    pos++;
+    return new_node('/', lhs, mul());
+  }
+
+  return lhs;
+}
+
+Node *term() {
+  if (tokens[pos].ty == TK_NUM)
+    return new_node_num(tokens[pos++].val);
+
+  if (tokens[pos].ty == '(') {
+    pos++;
+    Node *node = expr();
+    if (tokens[pos].ty == ')')
+      error("開きカッコに対応する閉じカッコがありません： %s",
+            tokens[pos].input);
+    pos++;
+    return node;
+  }
+
+  error("数値でも開きカッコでもないトークンです： %s", tokens[pos].input);
+
+  return (Node *)NULL;
+}
+
 // エラーを報告するための関数
-void error(int i) {
-  fprintf(stderr, "予期せぬトークンです： %s\n", tokens[i].input);
+void error(char *m, char *p) {
+  fprintf(stderr, m, p);
   exit(1);
+}
+
+void gen(Node *node) {
+  if (node->ty == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->ty) {
+  case '+':
+    printf("  add rax, rdi\n");
+    break;
+  case '-':
+    printf("  sub rax, rdi\n");
+    break;
+  case '*':
+    printf("  mul rdi\n");
+    break;
+  case '/':
+    printf("  mov rdx, 0\n");
+    printf("  div rdi\n");
+  }
+
+  printf("  push rax\n");
 }
 
 int main(int argc, char **argv) {
@@ -68,43 +185,20 @@ int main(int argc, char **argv) {
 
   // トークナイズする
   tokenize(argv[1]);
+  Node *node = expr();
 
+  //    アセンブリの前半部分を出力
   printf(".intel_syntax noprefix\n");
   printf(".global _main\n");
   printf("_main:\n");
 
-  // 式の最初は数でなければならないので、それをチェックして
-  // 最初のmov命令を出力
-  if (tokens[0].ty != TK_NUM)
-    error(0);
-  printf("  mov rax, %d\n", tokens[0].val);
+  //    抽象構文木を下りながらコードを生成
+  gen(node);
 
-  // `+ <数>`あるいは`-<数>`というトークンの並びを消費しつつ
-  // アセンブリを出力
-  int i = 1;
-
-  while (tokens[i].ty != TK_EOF) {
-    if (tokens[i].ty == '+') {
-      i++;
-      if (tokens[i].ty != TK_NUM)
-        error(i);
-      printf("    add rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-
-    if (tokens[i].ty == '-') {
-      i++;
-      if (tokens[i].ty != TK_NUM)
-        error(i);
-      printf("    sub rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-
-    error(i);
-  }
-
-  printf("    ret\n");
+  //    スタックトップに式全体の値が残っているはずなので
+  //    それをRAXにロードして関数からの返り値とする
+  printf("  pop rax\n");
+  printf("  ret\n");
   return 0;
+
 }
--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ try() {
     input="$2"
 
     ./9cc "$input" > tmp.s
+    cat tmp.s
     gcc -o tmp tmp.s
     ./tmp
     actual="$?"
@@ -17,5 +18,10 @@ try 0 0
 try 42 42
 try 21 '5+20-4'
 try 41 ' 12 + 34 - 5 '
+try 5 ' 1 + 2 + 3 + 4 - 5 '
+try 255 ' 10 + 20 + 30 + 40 + 205 - 50 '
+try 47 '5+6*7'
+try 15 '5*(9-6)'
+try 5  '(3+5)/2'
 
 echo OK
